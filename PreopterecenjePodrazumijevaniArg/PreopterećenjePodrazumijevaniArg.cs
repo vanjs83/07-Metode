902531989fcc12097a97bbf2fad80168dc2ea06c
vanjs83 <tihomir.vanjurek@gmail.com>
2015-12-03 19:53:03 +0100

    PreopterecenjePodrazumijevaniArg solve

--- PreopterecenjePodrazumijevaniArg/PreopterećenjePodrazumijevaniArg.cs
+++ PreopterecenjePodrazumijevaniArg/PreopterećenjePodrazumijevaniArg.cs
@@ -4,15 +4,18 @@ namespace Vsite.CSharp
 {
     class Program
     {
-        // TODO: Pokrenut program i kopirati ispis u datoteku PreopterecenjePodrazumijevaniArg.txt priloženu projektu
+        //  Pokrenut program i kopirati ispis u datoteku PreopterecenjePodrazumijevaniArg.txt priloženu projektu
 
         public static int Zbroji(int x = 3, int y = 4) 
         {
             return x + y;
         }
 
-        // TODO: Dodati definiciju funkcije public static int Zbroji(int x) i ponoviti pokretanje programa te usporediti ispise
-
+        //  Dodati definiciju funkcije public static int Zbroji(int x) i ponoviti pokretanje programa te usporediti ispise
+         public static int Zbroji(int x )
+        {
+            return x + 1;
+        }
         
         
         static void Main(string[] args)
