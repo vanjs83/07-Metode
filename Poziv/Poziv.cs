55d092c375dc6b17c66db5f1179349f1fc8f1328
vanjs83 <tihomir.vanjurek@gmail.com>
2015-12-03 19:39:05 +0100

    Poziv solve

--- Poziv/Poziv.cs
+++ Poziv/Poziv.cs
@@ -5,7 +5,7 @@ namespace Vsite.CSharp
     // ilustracija kako se razlučuje metoda koja će biti pozvana
     class MojaKlasa
     {
-        void Metoda1()
+       public void Metoda1()
         {
             Console.WriteLine("Metoda1()");
         }
@@ -21,8 +21,11 @@ namespace Vsite.CSharp
         static void Main(string[] args)
         {
             MojaKlasa mk = new MojaKlasa();
-            // TODO: Napisati poziv člana Metoda1 i člana Metoda2 s proslijeđenim cijelim brojem te izvesti program 
+            //  Napisati poziv člana Metoda1 i člana Metoda2 s proslijeđenim cijelim brojem te izvesti program 
+            mk.Metoda1();
+            mk.Metoda2(2);
 
+            
 
             Console.WriteLine("GOTOVO!!!");
             Console.ReadKey();
