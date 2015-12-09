f16b48eda002e68c442b1b0ab58ab05aa793f3c7
vanjs83 <tihomir.vanjurek@gmail.com>
2015-12-09 11:07:18 +0100

    New3 solve

--- new3/new3.cs
+++ new3/new3.cs
@@ -4,17 +4,58 @@ namespace Vsite.CSharp
 {
     // Primjer korištenja modifikatora new 
 
-    // TODO: Prekopirati definicije klasa Bazna, Izvedena i NajIzvedena iz projekta new2. 
-    // TODO: U klasu Bazna dodati virtualnu metodu void IspišiMiČlanove() te provjeriti što javlja prevoditelj
-    // TODO: Dodati modifikator new u definiciji metode Izvedena.IspišiMiČlanove
+    //  Prekopirati definicije klasa Bazna, Izvedena i NajIzvedena iz projekta new2. 
+    //  U klasu Bazna dodati virtualnu metodu void IspišiMiČlanove() te provjeriti što javlja prevoditelj
+    //  Dodati modifikator new u definiciji metode Izvedena.IspišiMiČlanove
 
+    class Bazna
+    {
+        public virtual void PredstaviSe()
+        {
+            Console.WriteLine("Bazna klasa");
+        }
+        public virtual void IspišiMiČlanove() 
+        {
+            Console.WriteLine("IspišiMiČlanove Bazna klasa");
+        }
+    }
+
+    class Izvedena : Bazna
+    {
+        public override void PredstaviSe()
+        {
+            Console.Write("Izvedena klasa, izvedena iz: ");
+            base.PredstaviSe();
+            IspišiMiČlanove();
+        }
+
+        protected  new void IspišiMiČlanove()
+        {
+            Console.WriteLine("Izvedena.IspišiMiČlanove");
+        }
+    }
+
+    class NajIzvedena : Izvedena
+    {
+        public override void PredstaviSe()
+        {
+            Console.Write("Još izvedenija klasa, izvedena iz: ");
+            base.PredstaviSe();
+        }
+
+        //  Dodati metodu IspišiMiČlanove koja će ispisati "NajIzvedena.IspišiMiČlanove"
+        public new void IspišiMiČlanove()
+        {
+            Console.WriteLine("NajIzvedena.IspišiMIČlanove");
+        }
+    }
 
 
     class Program
     {
         static void Main(string[] args)
         {
-            // TODO: Pokrenuti program, prekopirati ispis u datoteku new3.txt te dopisati obrazloženje
+            //  Pokrenuti program, prekopirati ispis u datoteku new3.txt te dopisati obrazloženje
 
             Bazna[] niz = new Bazna[] { new Bazna(), new Izvedena(), new NajIzvedena() };
 
